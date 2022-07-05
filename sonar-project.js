d008199c5231fecb789b48c6629622ce4e533ecd
Steven Adewole <106304974+Eutechsystems@users.noreply.github.com>
2022-07-05 18:39:40 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.59.54:9000/',
+  serverUrl: 'http://172.31.90.67:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.projectKey':'NodeJsKongitTech',
+	    'sonar.login': 'aa780f728cda5d7eda3a971e9e0225d651eb25eb',
+	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
